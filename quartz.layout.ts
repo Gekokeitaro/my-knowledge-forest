c73212ec4f0098522f090134e33b20b2174e8808
Gekokeitaro <pablo.bugarin.camara@gmail.com>
2024-01-05 21:40:49 +0100

    Implementación de la jerarquía de Dendron en el componente Explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { FileNode } from "./quartz/components/ExplorerNode";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -26,7 +27,60 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      
+      folderClickBehavior: "link",
+      sortFn: (a,b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          return a.children.length > b.children.length ? -1 : 1;
+        }
+        if (a.file && !b.file) {
+          return -1
+        } else {
+          return 1
+        }
+      },
+      mapFn: (node) => {
+
+        const CONTENT = "content/";
+        const MD = ".md";
+
+        if(node.name !== "") return;
+
+        const rootNodes : FileNode[] = [];
+
+        node.children.forEach( child => {
+
+          if(!child.file || !child.file.filePath) return;
+
+          let parent : FileNode | undefined = undefined;
+          let childDepth = child.file.filePath.split('/').pop()?.split('.').slice(0,-1);
+
+          if(typeof childDepth !== "undefined" && childDepth.length > 1) {
+            
+            child.depth = childDepth.length;
+
+            while(childDepth.length > 1){
+
+              childDepth.pop();
+              const parentPath = CONTENT + childDepth.join('.') + MD;
+              parent = node.children.find((parent) => {
+                return parent.file && parent.file.filePath === parentPath;
+              });
+
+              if(typeof parent !== "undefined") {
+                parent.children.push(child);
+                return;
+              }
+            }
+          }
+
+          if(!parent) rootNodes.push(child);
+        })
+
+        node.children = rootNodes;
+      }
+    })),
   ],
   right: [
     Component.Graph(),

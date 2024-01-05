2a552461bddb4a2fb00001304effdc3f3c39c072
Gekokeitaro <pablo.bugarin.camara@gmail.com>
2024-01-05 21:39:57 +0100

    Configuración

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gekokeitaro.github.io/my-knowledge-forest",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {

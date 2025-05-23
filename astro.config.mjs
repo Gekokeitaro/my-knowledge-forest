fe128ae698b78c9b4394bf3def50c0ca010bd08a
PBugarin <pablo.bugarin.camara@gmail.com>
2025-05-23 17:36:49 +0200

    Feature - Added rehypeCallouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import rehypeCallouts from 'rehype-callouts';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeCallouts]
+  },
 });

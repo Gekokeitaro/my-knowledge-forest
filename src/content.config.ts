412d0b14805ba7a5157b4a97d2fd6052840e2b94
PBugarin <pablo.bugarin.camara@gmail.com>
2025-05-30 17:19:12 +0200

    FEATURE - Notes collection first implementation

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const notes = defineCollection({
+    loader: glob({
+        pattern: '**/*.md',
+        base: './src/content/notes',
+    }),
+    schema: z.object({
+        id: z.string(),
+        title: z.string(),
+        created: z.date(),
+        modified: z.date(),
+        imgs: z.array(z.string()),
+        tags: z.array(z.string()),
+        alias: z.array(z.string()),
+    }),
+});
+
+export const collections = { notes };

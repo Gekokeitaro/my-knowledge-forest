9edcaf9ec530da50d40fe48025f945bf9df9f8d2
PBugarin <pablo.bugarin.camara@gmail.com>
2025-06-03 22:40:18 +0200

    FEATURE - React dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import tailwindcss from '@tailwindcss/vite';
 import rehypeCallouts from 'rehype-callouts';
 import astroRehypeRelativeMarkdownLinks from 'astro-rehype-relative-markdown-links';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     remarkPlugins: [],
     //rehypePlugins: [[rehypeCallouts, { theme: "github" }]] --> Para anotar después
     rehypePlugins: [rehypeCallouts, astroRehypeRelativeMarkdownLinks],
   },
+
+  integrations: [react()],
 });
